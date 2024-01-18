e40de70ba70fa54c4597ebf55f2d21b034cbbde1
MicZhang <562710164@qq.com>
2024-01-18 19:41:49 +0800

    Create xmake.lua

---
+++ xmake.lua
@@ -0,0 +1,11 @@
+package("opencv_android")
+   if is_plat("android") then
+        set_urls("https://github.com/opencv/opencv/releases/download/$(version)/opencv-$(version)-android-sdk.zip")
+        add_versions("4.9.0", "d181481fdabf6c02b1b45a3b382a583c531bb4d6b0582d0fbbb686487f071218")
+    end
+
+    on_install("android", function (package)
+        os.cp("sdk/native/jni/include", package:installdir())
+        os.cp("sdk/native/libs/$(arch)/*.so", package:installdir("lib"))
+    -- os.cp("lib/$(arch)/*.dll", package:installdir("lib"))
+    end)
