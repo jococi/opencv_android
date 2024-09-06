779a0491592d602940ccc44377495728fc20e6e3
miczhang <562710164@qq.com>
2024-09-06 14:50:31 +0800

    add opencv-mobile

--- packages/o/opencv-mobile/xmake.lua
+++ packages/o/opencv-mobile/xmake.lua
@@ -14,6 +14,8 @@ add_configs("shared", { description = "Build shared library.", default = false, 
 
 add_deps("cmake")
 
+
+
 on_install(function(package)
     local configs = {
         "-DBUILD_ZLIB=OFF",
@@ -115,7 +117,11 @@ on_install(function(package)
         table.insert(configs, "-DBUILD_opencv_world=OFF")
     elseif package:is_plat("android") then
         table.insert(configs, "-DBUILD_opencv_world=OFF")
+        table.insert(configs, "-DOPENCV_DISABLE_FILESYSTEM_SUPPORT=ON")
         table.insert(configs, "-DCMAKE_POLICY_DEFAULT_CMP0057=NEW")
+        if package:is_arch("arm64-v8a") then
+            table.insert(configs, "-DOPENCV_EXTRA_FLAGS=\"-mno-outline-atomics\"")
+        end
     end
 
     import("package.tools.cmake").install(package, configs)
